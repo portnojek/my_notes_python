a25050ecd14f16e85213adc0d30e05350ffbb8b3
Bartek Kocięcki <bartosz.kociecki@gmail.com>
2020-11-23 13:52:24 +0100

    update commit

--- testy/testing_commands.py
+++ testy/testing_commands.py
@@ -1,10 +1,14 @@
 import pytest
+
 def func(x):
-    return x + 1
+    return x + 0.1
 
 def test_answer():
-    assert func(3) == 5
+    assert func(4.9) == 5
+
+def test_approx():
+    assert func(5) == 5.1
+
 
 # wywołanie funkcji w pytest 'python -m pytest testing_commands.py'
-# w normalnym świecie wyglądałoby to poprostu 'pyton + plik
-'przydane strony'
+# w normalnym świecie wyglądałoby to poprostu 'pyton + plik'
